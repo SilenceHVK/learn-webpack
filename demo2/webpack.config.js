d84282ca2089e43094a4171287802ff5813a039b
hvkcoder <hvkcoder@163.com>
2017-12-22 08:19:12 +0800

    Update Demo2

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -5,12 +5,17 @@ const path = require('path')
 module.exports = {
     entry: './main.js',
     output: {
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
         rules: [
-          { test: /\.css$/, use: 'css-loader' }
+            {
+                test: /\.css$/, use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            }
         ]
     }
 }
