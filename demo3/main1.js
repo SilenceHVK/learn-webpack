7ea71307a9328221e58a61ffb1659d92afcff375
hvkcoder <hvkcoder@163.com>
2017-12-24 00:22:56 +0800

    Add demo3

---
+++ demo3/main1.js
@@ -0,0 +1,5 @@
+import './style.css'
+
+var h1 = document.createElement('h1');
+h1.innerHTML = '这是 main1.js 中的内容';
+document.body.appendChild(h1);
---
+++ demo3/main2.js
@@ -0,0 +1,5 @@
+import './style.css'
+
+var h2 = document.createElement('h2');
+h2.innerHTML = '这是 main2.js 中的内容';
+document.body.appendChild(h2);
