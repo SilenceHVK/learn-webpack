04a1f6683f896092a350f728fef742837c02e992
hvkcoder <hvkcoder@163.com>
2017-12-24 12:57:16 +0800

    Update demo3

--- demo3/dist/bundle1.js
+++ demo3/dist/bundle1.js
@@ -668,7 +668,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 var h1 = document.createElement('h1');
-h1.innerHTML = '这是 main1.js 中的内容';
+h1.innerText = '这是 main1.js 中的内容';
 document.body.appendChild(h1);
 
 /***/ })
